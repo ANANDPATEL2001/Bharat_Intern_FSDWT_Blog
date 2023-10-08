27dbe8e76690d5f5af5d22091115c4e84808e923
ANANDPATEL2001 <anand2010098@akgec.ac.in>
2023-10-08 15:46:34 +0530

    Back to basic 2

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"))
 });
 
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"))
+});
+
 app.get("/editor", (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"))
 });
